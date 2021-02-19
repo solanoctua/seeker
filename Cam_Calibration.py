dcbec709b1e7ea07aa8a3ec09688effe3d530f7e
solanoctua <79167361+solanoctua@users.noreply.github.com>
2021-02-19 23:39:37 +0300

    Update Cam_Calibration.py

--- Cam_Calibration.py
+++ Cam_Calibration.py
@@ -5,8 +5,20 @@ import os
 
 # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_calib3d/py_calibration/py_calibration.html#calibration
 # http://www.bim-times.com/opencv/3.3.0/d9/d0c/group__calib3d.html#ga3207604e4b1a1758aa66acb6ed5aa65d
-edge_of_square = 0.024 # in meters
+edge_of_square = 24 # in milimeters
+"""
+https://answers.opencv.org/question/39443/python-camera-calibration-square-size-seems-to-have-no-effect/
+Actually, camera matrix and distortion coefficients don't depend on square size.
+It makes sense: they are intrinsic parameters of a camera and they don't depend on particular images which you show to the camera.
+However, poses of the camera do depend on square size.
+Again it makes sense: if a chessboard filled the whole image but square size is very small then it means the camera is very close to the pattern. 
+But if square size is large then it means the camera is farther from the pattern.
+if you want to get just intrinsics (camera matrix and distortion) it doesn't matter which square size you specify (you can use just the default value).
+If you want to get extrinsics (rvecs and tvecs, that is poses of a camera) then you should specify accurate square size to get valid results.
+author:ilya-lysenkov (https://twitter.com/ilysenkov)
 
+that is, line 111 is important but line 34 is not for our purposes
+"""
 # termination criteria:
 # cv.TERM_CRITERIA_EPS - stop the algorithm iteration if specified accuracy, epsilon, is reached.
 # cv.TERM_CRITERIA_MAX_ITER - stop the algorithm after the specified number of iterations, max_iter.
@@ -96,8 +108,9 @@ def draw(img, corners, imgpts):
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((6*9,3), np.float32)
 objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+objp = objp * edge_of_square # to find location of corners in real world we multiply the matrix with edge_of_square
 
-axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
+axis = np.float32([[edge_of_square*3,0,0], [0,edge_of_square*3,0], [0,0,-3*edge_of_square]]).reshape(-1,3)
 count = 0
 for fname in images:
     img = cv2.imread(fname)
